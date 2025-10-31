90cbee40cdc772b04ff38076e4e0f1c39b641823
coyote321 <cochen8@s.sfusd.edu>
2025-10-31 14:51:01 -0700

    M7L5

---
+++ M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -0,0 +1,26 @@
+extends Control
+@onready var next_button: Button = %NextButton
+@onready var rich_text_label: RichTextLabel = %RichTextLabel
+
+var dialogue_items : Array[String] = [
+	"Hey Bob",
+	"How is your day going",
+	"im been having a great day",
+	"And so are you",
+]
+var current_item_index := 0
+
+func _ready() -> void:
+	show_text()
+	next_button.pressed.connect(advance)
+
+func show_text() -> void:
+	var current_item := dialogue_items[current_item_index]
+	rich_text_label.text = current_item
+
+func advance() -> void:
+	current_item_index += 1
+	if current_item_index == dialogue_items.size():
+		get_tree().quit()
+	else:
+		show_text()
